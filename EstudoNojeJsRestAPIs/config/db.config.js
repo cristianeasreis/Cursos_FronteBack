87ffa307fd40d7a3b2559e71b0cb5251b656f438
Cristiane Araujo Souza Dos Reis <cristianeasreis@hotmail.com>
2023-08-12 09:33:51 -0300

    corrigindo configuração de db

--- EstudoNojeJsRestAPIs/config/db.config.js
+++ EstudoNojeJsRestAPIs/config/db.config.js
@@ -1,4 +1,4 @@
-const { createPool } = require("mysql2/promise");
+const { createPool } = require("mysql2");
 /** Connection pool creation - START */
 const db = createPool({
   port: 3306,
@@ -6,8 +6,16 @@ const db = createPool({
   user: "CrisEstudos",
   password: "israel01061985",
   database: "heroku_12f1937794459fe",
+  waitForConnections: true,
+  connectionLimit: 10,
+  maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+  idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+  queueLimit: 0,
+  enableKeepAlive: true,
+  keepAliveInitialDelay: 0
   
 });
+
 /** Connection pool creation - END */
 
 module.exports = db;
