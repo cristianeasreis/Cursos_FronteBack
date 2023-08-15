0181e39e93588414317332b536b9a0b22ee72773
Cristiane Araujo Souza Dos Reis <cristianeasreis@hotmail.com>
2023-08-15 14:11:48 -0300

    finalizado a parte do login

--- EstudoNojeJsRestAPIs/controllers/users.controller.js
+++ EstudoNojeJsRestAPIs/controllers/users.controller.js
@@ -1,6 +1,7 @@
 const usersService = require("../services/users.service");
 
 exports.register = (req, res, next) => {
+    // validation area
     const data = {
         firstName: req.body.firstName,
         lastName: req.body.lastName,
@@ -11,7 +12,24 @@ exports.register = (req, res, next) => {
     usersService.register(data, (error, results) => {
         if (error) {
             console.log(error);
-            return res.status(400).send({ success: 0, data: "Servidor não encontrado" });
+            return res.status(400).send({ success: 0, data: "Bad request" });
+        }
+        return res.status(201).send({ success: 1,data: results,});
+      
+    });
+};
+
+exports.login = (req, res, next) => {
+    // validation area
+    const data = {
+        emailId: req.body.emailId,
+        password: req.body.password,
+
+    };
+    usersService.login(data, (error, results) => {
+        if (error) {
+            console.log(error);
+            return res.status(400).send({ success: 0, data: "Bad request" });
         }
         return res.status(201).send({ success: 1,data: results,});
       
